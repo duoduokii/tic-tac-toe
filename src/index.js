67ae06426f9f2214fe14dfdbc7d9961f13ad429a
duoduokii <2371233951@qq.com>
2020-11-26 23:00:16 +0800

    feat:添加历史记录列表

--- src/index.js
+++ src/index.js
@@ -90,25 +90,50 @@ class Game extends React.Component {
 					squares: Array(9).fill(null),
 				},
 			],
+			stepNumber: 0,
 			xIsNext: true,
 		};
 	}
 
 	handleClick(i) {
-		const history = this.state.history;
+		const history = this.state.history.splice(0, this.state.stepNumber + 1);
 		const current = history[history.length - 1];
 		const squares = current.squares.slice();
 		if (calculateWinner(squares) || squares[i]) {
 			return;
 		}
 		squares[i] = this.state.xIsNext ? 'X' : 'O';
-		this.setState({ history: history.concat([{ squares: squares }]), xIsNext: !this.state.xIsNext });
+		this.setState({
+			history: history.concat([{ squares: squares }]),
+			stepNumber: history.length,
+			xIsNext: !this.state.xIsNext,
+		});
+	}
+
+	jumpTo(step) {
+		this.setState({
+			stepNumber: step,
+			xIsNext: step % 2 === 0,
+		});
 	}
 
 	render() {
 		const history = this.state.history;
-		const current = history[history.length - 1];
+		console.log(this.state.stepNumber);
+		const current = history[this.state.stepNumber];
 		const winner = calculateWinner(current.squares);
+
+		const moves = history.map((step, move) => {
+			const desc = move ? 'Go to move #' + move : 'Go to game start';
+			return (
+				<li key={move}>
+					<button onClick={() => this.jumpTo(move)}>{desc}</button>
+				</li>
+			);
+		});
+
+		console.log(moves);
+
 		let status;
 		let isOver = true;
 		current.squares.forEach(item => {
@@ -123,13 +148,12 @@ class Game extends React.Component {
 		}
 		return (
 			<div className="game">
-				{/* <div className="status">{status}</div> */}
 				<div className="game-board">
 					<Board squares={current.squares} onClick={i => this.handleClick(i)} />
 				</div>
 				<div className="game-info">
 					<div>{status}</div>
-					<ol>{/* TODO */}</ol>
+					<ol>{moves}</ol>
 				</div>
 			</div>
 		);
