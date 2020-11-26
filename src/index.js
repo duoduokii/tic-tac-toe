92c3458009ab72b3771ac415e367b78524cc8b17
duoduokii <2371233951@qq.com>
2020-11-26 22:27:14 +0800

    feat(Game): 添加历史记录状态

--- src/index.js
+++ src/index.js
@@ -15,52 +15,51 @@ class Square extends React.Component {
 
 //9个小方格
 class Board extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			squares: new Array(9).fill(null),
-			xIsNext: true,
-		};
-	}
+	// constructor(props) {
+	// 	super(props);
+	// 	this.state = {
+	// 		squares: new Array(9).fill(null),
+	// 		xIsNext: true,
+	// 	};
+	// }
 	renderSquare(i) {
 		return (
 			// ?
 			<Square
-				value={this.state.squares[i]}
+				value={this.props.squares[i]}
 				onClick={() => {
-					this.handleClick(i);
+					this.props.onClick(i);
 				}}
 			/>
 		);
 	}
 
-	handleClick(i) {
-		const squares = this.state.squares.slice();
-		if (calculateWinner(squares) || squares[i]) {
-			return;
-		}
-		squares[i] = this.state.xIsNext ? 'X' : 'O';
-		this.setState({ squares: squares, xIsNext: !this.state.xIsNext });
-	}
+	// handleClick(i) {
+	// 	const squares = this.state.squares.slice();
+	// 	if (calculateWinner(squares) || squares[i]) {
+	// 		return;
+	// 	}
+	// 	squares[i] = this.state.xIsNext ? 'X' : 'O';
+	// 	this.setState({ squares: squares, xIsNext: !this.state.xIsNext });
+	// }
 
 	render() {
-		const winner = calculateWinner(this.state.squares);
-		let status;
-		let isOver = true;
-		this.state.squares.forEach(item => {
-			!item && (isOver = false);
-		});
-		if (winner) {
-			status = 'Winner:' + winner;
-		} else if (isOver) {
-			status = 'Game Over! 旗鼓相当的对手！';
-		} else {
-			status = 'Next Player:' + (this.state.xIsNext ? 'X' : 'O');
-		}
+		// const winner = calculateWinner(this.state.squares);
+		// let status;
+		// let isOver = true;
+		// this.state.squares.forEach(item => {
+		// 	!item && (isOver = false);
+		// });
+		// if (winner) {
+		// 	status = 'Winner:' + winner;
+		// } else if (isOver) {
+		// 	status = 'Game Over! 旗鼓相当的对手！';
+		// } else {
+		// 	status = 'Next Player:' + (this.state.xIsNext ? 'X' : 'O');
+		// }
 
 		return (
 			<div>
-				<div className="status">{status}</div>
 				<div className="board-row">
 					{this.renderSquare(0)}
 					{this.renderSquare(1)}
@@ -83,14 +82,53 @@ class Board extends React.Component {
 
 //带有默认值的棋盘
 class Game extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			history: [
+				{
+					squares: Array(9).fill(null),
+				},
+			],
+			xIsNext: true,
+		};
+	}
+
+	handleClick(i) {
+		const history = this.state.history;
+		const current = history[history.length - 1];
+		const squares = current.squares.slice();
+		if (calculateWinner(squares) || squares[i]) {
+			return;
+		}
+		squares[i] = this.state.xIsNext ? 'X' : 'O';
+		this.setState({ history: history.concat([{ squares: squares }]), xIsNext: !this.state.xIsNext });
+	}
+
 	render() {
+		const history = this.state.history;
+		const current = history[history.length - 1];
+		const winner = calculateWinner(current.squares);
+		let status;
+		let isOver = true;
+		current.squares.forEach(item => {
+			!item && (isOver = false);
+		});
+		if (winner) {
+			status = 'Winner:' + winner;
+		} else if (isOver) {
+			status = 'Game Over! 旗鼓相当的对手！';
+		} else {
+			status = 'Next Player:' + (this.state.xIsNext ? 'X' : 'O');
+		}
 		return (
 			<div className="game">
+				{/* <div className="status">{status}</div> */}
 				<div className="game-board">
-					<Board />
+					<Board squares={current.squares} onClick={i => this.handleClick(i)} />
 				</div>
 				<div className="game-info">
-					<div>{/* status */}</div>
+					<div>{status}</div>
 					<ol>{/* TODO */}</ol>
 				</div>
 			</div>
